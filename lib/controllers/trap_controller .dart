0157c5967e21907a401f7a7f33f48e1ac13576ed
abdallahHaider <123006440+abdallahHaider@users.noreply.github.com>
2024-08-13 20:41:49 +0300

    okaokdw

--- lib/controllers/trap_controller .dart
+++ lib/controllers/trap_controller .dart
@@ -6,93 +6,110 @@ import 'package:admin/models/transport.dart';
 import 'package:admin/screens/widgets/snakbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 
 import '../models/trap.dart';
 
-class TrapController extends ChangeNotifier{
-
-  List transports=[
+class TrapController extends ChangeNotifier {
+  List transports = [
     Transport(name: "بري", id: "street"),
     Transport(name: "جوي", id: "fly"),
   ];
 
-
-  Future<List> fetchData() async {
+  Future<Map> fetchData() async {
     // Simulate fetching data (replace with your actual logic)
     // await Future.delayed(Duration(seconds: 1));
-    
+
     try {
-      var x= await getpi("/api/trap/index?currency=IQD");
+      Map hap = {};
+      var x = await getpi("/api/trap/index?start_date=2024-08-12&end_date=2024-10-12");
       var data = jsonDecode(x.body)["data"];
       print(jsonDecode(x.body));
-      final List resellers=[];
-      for(int i=0;i<data.length;i++){
-        var x=Trap.fromJson(data[i]);
+      final List resellers = [];
+      for (int i = 0; i < data.length; i++) {
+        var x = Trap.fromJson(data[i]);
         resellers.add(x);
       }
-      return resellers;
+      hap["resellers"] = resellers;
+      hap['total_cost_USD'] = jsonDecode(x.body)['total_cost_USD'];
+      hap['total_cost_IQD'] = jsonDecode(x.body)['total_cost_IQD'];
+      hap['total_cost_RAS'] = jsonDecode(x.body)['total_cost_RAS'];
+      hap['total_cost_USD_pays'] = jsonDecode(x.body)['total_cost_USD_pays'];
+      hap['total_cost_IQD_pays'] = jsonDecode(x.body)['total_cost_IQD_pays'];
+      hap['total_cost_RAS_pays'] = jsonDecode(x.body)['total_cost_RAS_pays'];
+      hap['start_date'] = jsonDecode(x.body)['start_date'];
+      hap['end_date'] = jsonDecode(x.body)['end_date'];
+      hap['count_trap']=jsonDecode(x.body)['count_trap'];
+      hap['count_pays']=jsonDecode(x.body)['count_pays'];
+
+
+      return hap;
     } catch (e) {
       print(e);
       throw "jjj";
     }
-    
   }
-  Future addtrap(String duration,String quantity,String price_per_one,String RAS_to_USD,String IQD_to_USD,BuildContext context,String resslrid,String transportsid ,String trapid)async{
-  
+
+  Future addtrap(
+      String duration,
+      String quantity,
+      String price_per_one,
+      String RAS_to_USD,
+      String IQD_to_USD,
+      BuildContext context,
+      String resslrid,
+      String transportsid,
+      String trapid) async {
     http.Response x;
     try {
-      
-
       SmartDialog.showLoading();
-                  print('22222222222222');
+      print('22222222222222');
 
-     x =  await postApi("/api/trap/create", {
+      x = await postApi("/api/trap/create", {
         "hotel_id": trapid,
         "reseller_id": resslrid,
         "duration": duration,
-       "quantity": quantity,
-       "price_per_one": price_per_one,
-       "RAS_to_USD": RAS_to_USD,
-       "IQD_to_USD": IQD_to_USD,
-       "transport": transportsid,
+        "quantity": quantity,
+        "price_per_one": price_per_one,
+        "RAS_to_USD": RAS_to_USD,
+        "IQD_to_USD": IQD_to_USD,
+        "transport": transportsid,
       });
 
       print(x.body);
-      
     } catch (e) {
-        snackBar(context, e.toString());
+      snackBar(context, e.toString());
       throw e;
-    }finally{
+    } finally {
       SmartDialog.dismiss();
     }
-    if (x.statusCode==200||x.statusCode==201) {
-       notifyListeners();
+    if (x.statusCode == 200 || x.statusCode == 201) {
+      notifyListeners();
       snackBar(context, "تم اضافة الرحلة بنجاح");
-    }else{
+    } else {
       snackBar(context, jsonDecode(x.body));
     }
   }
-  Future delete(int id,BuildContext context)async{
+
+  Future delete(int id, BuildContext context) async {
     http.Response x;
     try {
       SmartDialog.showLoading();
-     x =  await postApi("/api/trap/delete", {
+      x = await postApi("/api/trap/delete", {
         "id": id,
-      
       });
-       notifyListeners();
-             Navigator.pop(context);
+      notifyListeners();
+      Navigator.pop(context);
 
       snackBar(context, "تم حذف الرحله بنجاح");
     } catch (e) {
-        snackBar(context, e.toString());
+      snackBar(context, e.toString());
       throw e;
-    }finally{
+    } finally {
       SmartDialog.dismiss();
     }
-    if (x.statusCode==200||x.statusCode==201) {
-    }else{
+    if (x.statusCode == 200 || x.statusCode == 201) {
+    } else {
       snackBar(context, jsonDecode(x.body));
     }
   }
--- lib/screens/trap/trap_page.dart
+++ lib/screens/trap/trap_page.dart
@@ -403,21 +403,127 @@ class TrapPage extends StatelessWidget {
                                 } else if (snapshot.hasError) {
                                   return ErorrWidget();
                                 } else if (snapshot.hasData) {
+                                  print(snapshot.data!);
                                   return Column(
                                     children: [
-                                      Container(
-                                        height: 150,
-                                        decoration: BoxDecoration(
-                                            color:
-                                                secondaryColor,
-                                            borderRadius:
-                                                BorderRadius.circular(10)),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                              color: secondaryColor,
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                          child: Column(
+                                            children: [
+                                              SizedBox(height: 10,),
+                                              Text('معلومات عامة'),
+                                              SizedBox(
+                                                height: defaultPadding,
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        horizontal: 12.0),
+                                                child: Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    Text(
+                                                        "التلكفة الاجمالية بالدولار : ${snapshot.data!['total_cost_USD'].toString()}"),
+                                                    Text(
+                                                        "التلكفة الاجمالية بالدينار : ${snapshot.data!['total_cost_IQD'].toString()}"),
+                                                    Text(
+                                                        "التلكفة الاجمالية بالريل السعودي : ${snapshot.data!['total_cost_RAS'].toString()}"),
+                                                  ],
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.symmetric(horizontal: 12),
+                                                child: Divider(),
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        horizontal: 12.0),
+                                                child: Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    Text(
+                                                        "المبلغ المدفوع بالدولار: ${snapshot.data!['total_cost_USD_pays'].toString()}"),
+                                                    Text(
+                                                        "المبلغ المدفوع بالدينار: ${snapshot.data!['total_cost_IQD_pays'].toString()}"),
+                                                    Text(
+                                                        "المبلغ المدفوع بالريل السعودي: ${snapshot.data!['total_cost_RAS_pays'].toString()}"),
+                                                  ],
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.symmetric(horizontal: 12),
+                                                child: Divider(),
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        horizontal: 12.0),
+                                                child: Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    Text(
+                                                      "الارباح بالدينار: ${(snapshot.data!['total_cost_IQD_pays'] - snapshot.data!['total_cost_IQD']).toString()}",
+                                                      style: TextStyle(
+                                                          color: snapshot.data![
+                                                                          'total_cost_IQD_pays'] -
+                                                                      snapshot.data![
+                                                                          'total_cost_IQD'] <
+                                                                  0
+                                                              ? Colors.red
+                                                              : Colors.green),
+                                                    ),
+                                                    Text(
+                                                      "الارباح بالريال: ${(snapshot.data!['total_cost_RAS_pays'] - snapshot.data!['total_cost_RAS']).toString()}",
+                                                      style: TextStyle(
+                                                          color: snapshot.data![
+                                                                          'total_cost_RAS_pays'] -
+                                                                      snapshot.data![
+                                                                          'total_cost_RAS'] <
+                                                                  0
+                                                              ? Colors.red
+                                                              : Colors.green),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.symmetric(horizontal: 12),
+                                                child: Divider(),
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.symmetric(horizontal: 12),
+                                                child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
+                                                  Text('عدد التسديدات :'+snapshot.data!['count_pays'].toString()),
+                                                 Text(
+                                                    "المدة الزمنية : ${snapshot.data!['start_date'] == null ? "لا يوجد" : " من " + snapshot.data!['start_date'] + " الى " + snapshot.data!['end_date']}"),
+                                                Text('عدد الرحلات :'+snapshot.data!['count_trap'].toString()),
+                                                ],),
+                                              ),
+                                              SizedBox(height: 10,)
+                                         
+                                            ],
+                                          ),
+                                        ),
                                       ),
                                       Expanded(
                                         child: ListView.builder(
-                                          itemCount: snapshot.data!.length,
+                                          itemCount: snapshot
+                                              .data!['resellers'].length,
                                           itemBuilder: (context, index) {
-                                            final trip = snapshot.data![index];
+                                            final trip = snapshot
+                                                .data!['resellers'][index];
                                             return Padding(
                                               padding:
                                                   const EdgeInsets.symmetric(
@@ -599,7 +705,8 @@ class TrapPage extends StatelessWidget {
                                                       Row(
                                                         children: [
                                                           CircleAvatar(
-                                                            backgroundColor: primaryColor,
+                                                            backgroundColor:
+                                                                primaryColor,
                                                             child: trip.transport ==
                                                                     'fly'
                                                                 ? SvgPicture
