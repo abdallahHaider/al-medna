caf0356936761a56219561697848dbffcc2d54af
Husseinl <34353391+Husseinl@users.noreply.github.com>
2024-09-08 15:08:07 +0300

    هع

--- lib/screens/dashboard/components/storage_details.dart
+++ lib/screens/dashboard/components/storage_details.dart
@@ -1,4 +1,7 @@
+import 'package:admin/controllers/action_bank_controller.dart';
 import 'package:admin/controllers/wallet_provider.dart';
+import 'package:admin/screens/bank/bank.dart';
+import 'package:admin/screens/widgets/action_bank_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -23,9 +26,14 @@ class StorageDetails extends StatelessWidget {
               Icons.account_balance,
               size: 100,
             ),
+            Text(
+              'الصندوق الرئيسي',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
             SizedBox(
               height: defaultPadding,
             ),
+            
             Text(
               'الرصيد بالدينار',
               style: TextStyle(fontSize: 18, color: Colors.grey),
@@ -71,9 +79,147 @@ class StorageDetails extends StatelessWidget {
                 ),
               ],
             ),
+            //المصرف/////////////////////////////////////////////////////////////////////
+            Padding(
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.account_balance,
+              size: 100,
+            ),
+            Text(
+              'المصرف',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            SizedBox(
+              height: defaultPadding,
+            ),
+            
+            Text(
+              'الرصيد بالدينار',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.money),
+                Consumer<WalletProvider>(
+                  builder: (context, storage, child) {
+                    return Text(
+                      '${storage.wallet_IQD}',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: defaultPadding,
+            ),
+            Text(
+              'الرصيد بالدولار',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.attach_money),
+                Consumer<WalletProvider>(
+                  builder: (context, storage, child) {
+                    return Text(
+                      '${storage.wallet_USD}',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            // البنك////////////////////////////////////////////////////////////
+            Padding(
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.account_balance,
+              size: 100,
+            ),
+                Text(
+              'البنك',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            SizedBox(
+              height: defaultPadding,
+            ),
+            
+            Text(
+              'الرصيد بالدينار',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.money),
+                Consumer<ActionBankController>(
+                  builder: (context, storage, child) {
+                    return Text(
+                      '${storage.isTo}',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: defaultPadding,
+            ),
+            Text(
+              'الرصيد بالدولار',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.attach_money),
+                Consumer<WalletProvider>(
+                  builder: (context, storage, child) {
+                    return Text(
+                      '${storage.wallet_USD}',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ],
+          
         ),
       ),
+          ],
+          
+        ),
+      ),
+          ],
+          
+        ),
+        
+      ),
+      
     );
+    
   }
 }
